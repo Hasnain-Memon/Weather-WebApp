2034d2599d8a87a0393b38c1bb781e1469734c40
Hasnain Memon <trendblend18@gmail.com>
2024-03-26 00:47:34 +0500

    adding all the all the try catch, function and variables inside search btn event listener

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=pakistan&appid=8ecfdb17a81d4fc60d5bb9ad93e48d53&units=metric";
+const searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', async (e)=>{
+    e.preventDefault();
+    const search = document.getElementById('searchInput');
+    const location = search.value;
 
-
-
-async function getResponse() {
-    try{
-        const response = await fetch(url);
-        if(!response.ok){
-            throw new Error('network respone was not ok');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8ecfdb17a81d4fc60d5bb9ad93e48d53&units=metric`
+    
+    async function getResponse() {
+        try{
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error('network respone was not ok');
+            }
+            const data = await response.json();
+            console.log(data);
+    
+            const temperature = document.getElementById('temp');
+            temperature.innerText = data.main.temp;
+    
+            const windSpeed = document.getElementById('wind');
+            windSpeed.innerText = data.wind.speed;
+    
+            const humidity = document.getElementById('humi');
+            humidity.innerText = data.main.humidity;
+    
+        } catch (error){
+            console.error('there was a problem with the fetch operation');
         }
-        const data = await response.json();
-        console.log(data);
-
-        const temperature = document.getElementById('temp');
-        temperature.innerText = data.main.temp;
-
-        const windSpeed = document.getElementById('wind');
-        windSpeed.innerText = data.wind.speed;
-
-        const humidity = document.getElementById('humi');
-        humidity.innerText = data.main.humidity;
-
-    } catch (error){
-        console.error('there was a problem with the fetch operation');
     }
-};
 
-getResponse();
+    getResponse();
+});
